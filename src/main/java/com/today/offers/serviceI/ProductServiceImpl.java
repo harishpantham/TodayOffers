dfd9962d74d260d941acfab4b87d9d8ef168514a
admin <harishpantham38@gmail.com>
2019-05-06 23:17:44 +0530

    changes in product service Impl

--- src/main/java/com/today/offers/serviceI/ProductServiceImpl.java
+++ src/main/java/com/today/offers/serviceI/ProductServiceImpl.java
@@ -2,7 +2,40 @@ package com.today.offers.serviceI;
 
 import org.springframework.stereotype.Service;
 
+import com.today.offers.dto.ProductDTO;
+import com.today.offers.utils.OffersResponse;
+
 @Service
-public class ProductServiceImpl {
+public class ProductServiceImpl implements ProductServiceI {
+
+	@Override
+	public OffersResponse getAllProducts() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public OffersResponse getProductById(Long productId) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public OffersResponse saveProduct(ProductDTO productDTO) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public OffersResponse updateProduct(ProductDTO productDTO) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public OffersResponse deleteProduct(Long productId) {
+		// TODO Auto-generated method stub
+		return null;
+	}
 
 }
